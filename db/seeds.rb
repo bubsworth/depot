74f7ff4903bda3dfb6c6cfdb63609b3087090dd0
Beth Hubbard <beth.hubbard@commit.digital>
2020-10-27 10:50:26 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.delete_all
-#....
-Product.create! (
-    title:'Build Chatbot Interactions',
-    description:
-        %{<p><em>This is a description for the item in question'</em> Thats about it tbh</p>},
-    image_url: 'dpchat.jpg',
+
+Product.create! (title:"Album 1",
+    description: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
+    image_url: 'sapphic_cover.jpg',
     price: 20.00)
-#....
     
