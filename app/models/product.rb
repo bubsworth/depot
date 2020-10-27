8ea17a30f19073d8030d6c988303e8e8d3388b39
Beth Hubbard <beth.hubbard@commit.digital>
2020-10-27 10:50:52 +0000

    Add ensure_not_referenced_by_any_line_item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,13 @@
 class Product < ApplicationRecord
     has_many :line_items
-
     before_destroy :ensure_not_referenced_by_any_line_item
-    
     #...
-
     private
-
-        # ensure that there are no lien items referencing this product
-        def ensure_not_referenced_by_any_line_item
-            unless line_items.empty?
-                errors.add(:base, 'Line Items present')
-                throw :abort
-            end
+    # ensure that there are no lien items referencing this product
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            throw :abort
         end
+    end
 end
