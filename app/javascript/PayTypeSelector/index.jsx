2077c588d23eb7b4d44e9a96fea1b2de7db29968
Beth Hubbard <Beth Hubbard>
2021-01-13 12:51:47 +0000

    Fix typo

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -6,7 +6,7 @@ class PayTypeSelector extends React.Component {
         <label htmlFor="order_pay_type">Pay Type</label>
         <select id="order_pa_type" name="order[pay_type">
           <option value="">Select a payment method</option>
-          <option value="Check">Check</option>
+          <option value="Cheque">Cheque</option>
           <option value="Credit Card">Credit card</option>
           <option value="Purchase order">Purchase order</option>
         </select>
