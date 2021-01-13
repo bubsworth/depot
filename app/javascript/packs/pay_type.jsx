0406ebcb142abdbe0ba4dfcae0f3b882c32dd666
Beth Hubbard <Beth Hubbard>
2021-01-13 09:32:03 +0000

    Adding component to rails code

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import PayTypeSelector from 'PayTypeSelector'
 
 document.addEventListener('turbolinks:load', function() {
-    var element = document.getElementById("pay-type-componeny");
-    ReactDOM.render(<PayTypeSelector />, element);
+    var element = document.getElementById("pay-type-component");
+    ReactDOM.render(<PayTypeSelector/>, element);
 });
