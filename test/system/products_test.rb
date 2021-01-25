912a902bf7cc65eb1cb8157dac5a8304d023a9d1
Beth Hubbard <bah1996@hotmail.co.uk>
2021-01-25 11:13:11 +0000

    Test fill_in "Title"

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -28,10 +28,10 @@ class ProductsTest < ApplicationSystemTestCase
     visit products_url
     click_on "Edit", match: :first
 
-    fill_in "title", with: @product.title
     fill_in "Description", with: @product.description
     fill_in "Image url", with: @product.image_url
     fill_in "Price", with: @product.price
+    fill_in "Title", with: "Karel The Robot in a Nutshell"
     click_on "Update Product"
 
     assert_text "Product was successfully updated"
