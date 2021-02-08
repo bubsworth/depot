15c79e16e0c61beb3f29e892dedf5ae944b4ddb2
Beth Hubbard <bah1996@hotmail.co.uk>
2021-02-08 17:03:08 +0000

    Start I18n implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   protected 
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I19n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+        logger.error flash.ow[:notice]
+      end
+    end
+  end 
+
   def authorize
     unless User.find_by(id: session[:user_id])
       redirect_to login_url, notice: "Please log in"
