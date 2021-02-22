8103493db33ec5ac6ab2c44b1be4dbbed74c23b5
Beth Hubbard <bah1996@hotmail.co.uk>
2021-02-22 15:19:44 +0000

    Translate storefront

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   def set_i18n_locale_from_params
     if params[:locale]
       if I18n.available_locales.map(&:to_s).include?(params[:locale])
-        I19n.locale = params[:locale]
+        I18n.locale = params[:locale]
       else
         flash.now[:notice] =
           "#{params[:locale]} translation not available"
-        logger.error flash.ow[:notice]
+        logger.error flash.now[:notice]
       end
     end
   end 
